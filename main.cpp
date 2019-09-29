53ae9f15c532b3f3145ae3d4022ad6deee62edb4
1447264892 <55919999+1447264892@users.noreply.github.com>
2019-09-29 19:30:13 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,261 +1,330 @@
 #include <iostream>
 #include <string.h>
+
 using namespace std;
 
 int main()
-{   char match[100][100];
-    char match2[100][100];
-    char phone[100][100];
-    char province[100][100];
-    char city[100][100];
-    char name[100][100];
-    char qu[100][100];
-    char street[100][100];
-    char address[100][100];
-    int i,j;
-    int k;
-    int n;
-    int m;
-    int f;
-    cin>>n;
-    int g=0;
-    for(i=0;i<n;i++)
-    {
-        cin>>match[i];
-    }
-    for(i=0;i<n;i++)//ÕÒ³öÊÖ»úºÅÂë
-    {   k=0;
-        for(j=0;j<strlen(match[i]);j++)
-        { f=0;
-           if(match[i][j]>='0'&&match[i][j]<='9')//ÕÒ³öµÚÒ»¸öÊý×Ö
-                {
-
-                  for(m=j;m<=j+11;m++)//ÅÐ¶Ï´ÓÕâ¸öÊý×Ö¿ªÊ¼ºóÊ®Ò»¸öÊý×ÖÊÇ·ñÎªÊÖ»úºÅÂë£¬Çø·ÖÊÖ»úºÅÂëºÍµØÖ·ºÅÂë£¬ÊÖ»úºÅÂë£ºÊ®Ò»Î»£¬µÚÊ®¶þÎ»²»¿ÉÄÜÊÇÊý×Ö¡£µØÖ·ºÅÂë£ºÎÞÏÞ¶àÎ»£ºÒþ»¼£ºÊÖ»úºÅÂëÈô·ÅÔÚ×îºó
-                  {//ÊÖ»úºÅÂë·ÅÔÚ×îºó²»ÄÜÕâÃ´ÓÃ
-                      if(match[i][m]>='0'&&match[i][m]<='9')
-                      {
-                          k++;
-                      }
-
-
-                  }
-                  if(k==11)//ÕÒµ½ÊÖ»úºÅÂë
-                  {   g=0;
-
-                      for(m=j;m<=j+10;m++)
-                      {
-                          phone[i][g]=match[i][m];
-                          g++;
-                      }
-                      g=0;
-                      for(f=0;f<=strlen(match[i]);f++)
-                      {if(f==j)
-                          {
-                              f+=11;
-                          }
-                          match2[i][g]=match[i][f];
-                          g++;
-
-
-                      }
-                      break;
-                  }
-                  else
-             {
-                 j=m;
-             }
-                }
-
-        }
-    }
-  //½ÓÏÂÀ´µØÖ··Ö½â
-  //ÏÈ¿¼ÂÇÖ±Ï½ÊÐ
-  char cf[100];
-  char cf2[100];
-  int z=0;
-  for(i=0;i<n;i++)
-  {
-      for(j=0;j<strlen(match2[i]);j++)
-      {g=0;
-          if(match2[i][j]==',')
-          {
-              for(g=0;g<j;g++)
-              {
-                  name[i][g]=match2[i][g];
-
-              }
-              break;
-          }
-
-
-          }
-          j++;
-          int k;
-          g=j;
-          for(;j<strlen(match2[i]);)
-          {
-            cf[0]=match2[i][j++];
-            cf[1]=match2[i][j++];
-            z=0;
-            if(strcmp(cf,"ÊÐ")==0)//ÕÒÊÐ¼¶
-            {cout<<j<<'\n';
-                for(k=g;k<j-2;k++)
-                {
-                    province[i][z++]=match2[i][k];
-
-                }
-                z=0;
-                for(k=g;k<j;k++)
-                {
-
-                    city[i][z++]=match2[i][k];
-                }
-                g=j;
-                break;
-            }
-            else
-            {
-                z=0;
-
-                  if(strcmp(cf,"Ê¡")==0)
-                  {
-                      for(;g<j;g++)
-                      {
-                          province[i][z++]=match2[i][g];
-
-                      }
-
-                      for(;j<strlen(match2[i]);)
-                      {
-                          cf[0]=match2[i][j++];
-                          cf[1]=match2[i][j++];
-                          z=0;
-
-                          if(strcmp(cf,"ÊÐ")==0)
-                          {
-                              for(;g<j;g++)
-                              {
-                                  city[i][z++]=match2[i][g];
-
-                              }
-                              break;
-                          }
-                          memset(cf2,0,sizeof(cf2));
-                      }
-                      break;
-                  }
-            }
-          memset(cf,0,sizeof(cf));
-          }
-          z=0;
-          memset(cf2,0,sizeof(cf2));
-          int o=0;
-
-          for(j=g;j<strlen(match2[i]);)
-            {z=0;
-              cf[0]=match2[i][j++];
-              cf[1]=match2[i][j++];
-
-              o=j;
-              cf2[0]=match2[i][o++];
-              cf2[1]=match2[i][o];
-              if(strcmp(cf,"Çø")==0||strcmp(cf,"ÏØ")==0||strcmp(cf,"ÊÐ")==0)
-              {
-                 for(k=g;k<j;k++)
-                 {
-                    qu[i][z++]=match2[i][k];
-                 }
-                 g=j;
-                 for(;j<strlen(match2[i]);)
-                 {
-                      memset(cf,0,sizeof(cf));
-                 memset(cf2,0,sizeof(cf2));
-                 cf[0]=match2[i][j++];
-              cf[1]=match2[i][j++];
-
-              o=j;
-              cf2[0]=match2[i][o++];
-              cf2[1]=match2[i][o];
-              z=0;
-              if(strcmp(cf,"Õò")==0||strcmp(cf,"Ïç")==0)
-              {
-                  for(k=g;k<j;k++)
-                    {
-                        street[i][z++]=match2[i][k];
-                    }
-                        z=0;
-                    for(k=j;k<strlen(match2[i]);k++)
-                    {
-                     address[i][z++]=match2[i][k];
-                    }
-                    break;
-
-              }
-              else if(strcmp(cf,"½Ö")==0&&strcmp(cf2,"µÀ")==0)
-              {
-                   for(k=g;k<j+2;k++)
-                    {
-                        street[i][z++]=match2[i][k];
-                    }
-                        z=0;
-                    for(k=j+2;k<strlen(match2[i]);k++)
-                    {
-                     address[i][z++]=match2[i][k];
-                    }
-                    break;
-              }
-              if(cf[0]>='0'&&cf[0]<='9')
-              {
-                  for(k=g;k<strlen(match2[i]);k++)
-                  {
-
-                      address[i][z++]=match2[i][k];
-                  }
-                  break;
-              }
-
-             if(j==strlen(match2[i]))
-                     {
-
-                         for(k=g;k<strlen(match2[i]);k++)
-                  {
-                      address[i][z++]=match2[i][k];
-
-                  }break;
-
-                     }
-
-                 }
-
-
-
-              break;
-              }
-              else if(strcmp(cf,"Õò")==0||(strcmp(cf,"½Ö")==0&&(strcmp(cf2,"µÀ")==0))||strcmp(cf,"Ïç")==0)
-              {
-                  for(k=g;k<j;k++)
-                    {
-                        street[i][z++]=match2[i][k];
-                    }
-                        z=0;
-                    for(k=j;k<strlen(match2[i]);k++)
-                    {
-                     address[i][z++]=match2[i][k];
-                    }
-                    break;
-
-              }
-          }
-
-  }
-  int l=strlen(address[i]);
-  for(i=0;i<n;i++)
-  {
-      address[i][l]='\0';
-  }
-for(i=0;i<n;i++)
 {
-    cout<<"{\"ÐÕÃû\".:\""<<name[i]<<"\",\"ÊÖ»ú\":\""<<phone[i]<<"\",\"µØÖ·\":[\""<<province[i]<<"\",\""<<city[i]<<"\",\""<<qu[i]<<"\",\""<<street[i]<<"\",\""<<address[i]<<"\"]}";
-}
-    return 0;
+	int y;
+	char match[100][100];
+	char match2[100][100];
+	char phone[100][100];
+	char province[100][100];
+	char city[100][100];
+	char name[100][100];
+	char qu[100][100];
+	char street[100][100];
+	char address[100][100];
+	int i, j;
+	int k;
+	int n;
+	int m;
+	int f;
+	char hard[100];
+	n = 1;
+	int g = 0;
+	cin >> hard[0];
+	cin >> hard[1];
+	for (i = 0; i < n; i++)
+	{
+		cin >> match[i];
+	}
+	for (i = 0; i < n; i++)//æ‰¾å‡ºæ‰‹æœºå·ç 
+	{
+		k = 0;
+		int y = strlen(match[i]);
+		for (j = 0; j < y; j++)
+		{
+			f = 0;
+			k = 0;
+
+			if (match[i][j] >= '0'&&match[i][j] <= '9')//æ‰¾å‡ºç¬¬ä¸€ä¸ªæ•°å­—
+			{
+				for (m = j; m <= j + 10; m++)//åˆ¤æ–­ä»Žè¿™ä¸ªæ•°å­—å¼€å§‹åŽåä¸€ä¸ªæ•°å­—æ˜¯å¦ä¸ºæ‰‹æœºå·ç ï¼ŒåŒºåˆ†æ‰‹æœºå·ç å’Œåœ°å€å·ç ï¼Œæ‰‹æœºå·ç ï¼šåä¸€ä½ï¼Œç¬¬åäºŒä½ä¸å¯èƒ½æ˜¯æ•°å­—ã€‚åœ°å€å·ç ï¼šæ— é™å¤šä½ï¼šéšæ‚£ï¼šæ‰‹æœºå·ç è‹¥æ”¾åœ¨æœ€åŽ
+				{//æ‰‹æœºå·ç æ”¾åœ¨æœ€åŽä¸èƒ½è¿™ä¹ˆç”¨
+
+					if (match[i][m] >= '0'&&match[i][m] <= '9')
+					{
+						k++;
+					}
+
+				}
+				if (k == 11)//æ‰¾åˆ°æ‰‹æœºå·ç 
+				{
+					g = 0;
+					for (m = j; m <= j + 10; m++)
+					{
+						phone[i][g] = match[i][m];
+						g++;
+					}
+					g = 0;
+					y = strlen(match[i]);
+					for (f = 0; f <= y; f++)
+					{
+						if (f == j)
+						{
+							f += 11;
+						}
+						match2[i][g] = match[i][f];
+						g++;
+					}
+					break;
+				}
+
+			}
+
+		}
+	}
+
+	//æŽ¥ä¸‹æ¥åœ°å€åˆ†è§£
+	//å…ˆè€ƒè™‘ç›´è¾–å¸‚
+	char cf[100];
+	char cf2[100];
+	int z = 0;
+	for (i = 0; i < n; i++)
+	{
+		y = strlen(match2[i]);
+		for (j = 0; j < y; j++)
+		{
+			g = 0;
+			if (match2[i][j] == ',')
+			{
+				for (g = 0; g < j; g++)
+				{
+					name[i][g] = match2[i][g];
+
+				}
+				break;
+			}
+
+
+		}
+		j++;
+		int k;
+		g = j;
+		y = strlen(match2[i]);
+		for (; j < y;)
+		{
+			cf[0] = match2[i][j++];
+			cf[1] = match2[i][j++];
+			z = 0;
+			if (strcmp(cf, "åŒº") == 0)
+			{
+				for (; g < j; g++)
+				{
+					province[i][z++] = match2[i][g];
+
+				}
+				y = strlen(match2[i]);
+				for (; j < y;)
+				{
+					cf[0] = match2[i][j++];
+					cf[1] = match2[i][j++];
+					z = 0;
+
+					if (strcmp(cf, "å¸‚") == 0)
+					{
+						for (; g < j; g++)
+						{
+							city[i][z++] = match2[i][g];
+
+						}
+						break;
+					}
+					memset(cf2, 0, sizeof(cf2));
+				}
+				break;
+			}
+			else if (strcmp(cf, "å¸‚") == 0)//æ‰¾å¸‚çº§
+			{
+				for (k = g; k < j - 2; k++)
+				{
+					province[i][z++] = match2[i][k];
+
+				}
+				z = 0;
+				for (k = g; k < j; k++)
+				{
+
+					city[i][z++] = match2[i][k];
+				}
+				g = j;
+				break;
+			}
+			else
+			{
+				z = 0;
+
+				if (strcmp(cf, "çœ") == 0)
+				{
+					for (; g < j; g++)
+					{
+						province[i][z++] = match2[i][g];
+
+					}
+					y = strlen(match2[i]);
+					for (; j < y;)
+					{
+						cf[0] = match2[i][j++];
+						cf[1] = match2[i][j++];
+						z = 0;
+
+						if (strcmp(cf, "å¸‚") == 0 || strcmp(cf, "å·ž") == 0)
+						{
+							for (; g < j; g++)
+							{
+								city[i][z++] = match2[i][g];
+
+							}
+							break;
+						}
+						memset(cf2, 0, sizeof(cf2));
+					}
+					break;
+				}
+			}
+			memset(cf, 0, sizeof(cf));
+		}
+		z = 0;
+		memset(cf2, 0, sizeof(cf2));
+		int o = 0;
+		y = strlen(match2[i]);
+		for (j = g; j < y;)
+		{
+			z = 0;
+			cf[0] = match2[i][j++];
+			cf[1] = match2[i][j++];
+
+			o = j;
+			cf2[0] = match2[i][o++];
+			cf2[1] = match2[i][o];
+			if (strcmp(cf, "åŒº") == 0 || strcmp(cf, "åŽ¿") == 0 || strcmp(cf, "å¸‚") == 0)
+			{
+				for (k = g; k < j; k++)
+				{
+					qu[i][z++] = match2[i][k];
+				}
+				g = j;
+				y = strlen(match2[i]);
+				for (; j < y;)
+				{
+					memset(cf, 0, sizeof(cf));
+					memset(cf2, 0, sizeof(cf2));
+					cf[0] = match2[i][j++];
+					cf[1] = match2[i][j++];
+
+					o = j;
+					cf2[0] = match2[i][o++];
+					cf2[1] = match2[i][o];
+					z = 0;
+
+					if (strcmp(cf, "é•‡") == 0 || strcmp(cf, "ä¹¡") == 0)
+					{
+						for (k = g; k < j; k++)
+						{
+							street[i][z++] = match2[i][k];
+						}
+						z = 0;
+						y = strlen(match2[i]);
+						for (k = j; k < y; k++)
+						{
+							address[i][z++] = match2[i][k];
+						}
+						break;
+
+					}
+					else if (strcmp(cf, "è¡—") == 0 && strcmp(cf2, "é“") == 0)
+					{
+						for (k = g; k < j + 2; k++)
+						{
+							street[i][z++] = match2[i][k];
+						}
+						z = 0;
+						y = strlen(match2[i]);
+						for (k = j + 2; k < y; k++)
+						{
+							address[i][z++] = match2[i][k];
+						}
+
+						break;
+					}
+
+					if (cf[0] >= '0'&&cf[0] <= '9')
+					{
+						for (k = g; k < y; k++)
+						{
+
+							address[i][z++] = match2[i][k];
+						}
+						break;
+					}
+
+					if ((j - 1) == y)
+					{
+
+						for (k = g; y; k++)
+						{
+							address[i][z++] = match2[i][k];
+
+						}
+						break;
+
+					}
+
+				}
+
+
+
+				break;
+			}
+			else  if (strcmp(cf, "é•‡") == 0 || strcmp(cf, "ä¹¡") == 0)
+			{
+				for (k = g; k < j; k++)
+				{
+					street[i][z++] = match2[i][k];
+				}
+				z = 0;
+				for (k = j; k < y; k++)
+				{
+					address[i][z++] = match2[i][k];
+				}
+				break;
+
+			}
+			else if (strcmp(cf, "è¡—") == 0 && strcmp(cf2, "é“") == 0)
+			{
+				for (k = g; k < j + 2; k++)
+				{
+					street[i][z++] = match2[i][k];
+				}
+				z = 0;
+
+				for (k = j + 2; k < y; k++)
+				{
+					address[i][z++] = match2[i][k];
+				}
+
+				break;
+			}
+		}
+
+	}
+	int l = strlen(address[0]);
+	for (i = 0; i < n; i++)
+	{
+		address[0][l - 1] = '\0';
+	}
+
+	for (i = 0; i < n; i++)
+	{  cout<<"{";
+		cout << "\"å§“å\":\"" << name[0] << "\",\"æ‰‹æœº\":\"";
+		for (j = 0; j < 11; j++)
+		{
+			cout << phone[0][j];
+		}
+			cout<< "\",\"åœ°å€\":[\"" << province[0] << "\",\"" << city[0] << "\",\"" << qu[0] << "\",\"" << street[0] << "\",\"" << address[0] << "\"]";
+			cout<<"}";
+	}
+
+	return 0;
 }
